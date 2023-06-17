a1a47292970c11204f297ad33177b197113cc504
Maz <mazspm@icloud.com>
2023-06-16 19:53:00 -0500

    sign in page done

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,10 +1,10 @@
-import { FC } from 'react'
 import Link from 'next/link'
 
-import { cn } from '@/lib/utils'
+import { SignIn } from '@/components/SignIn'
 import { buttonVariants } from '@/components/ui/Button'
+import { cn } from '@/lib/utils'
 
-const page: FC = ({}) => {
+const page = () => {
   return (
     <div className="absolute inset-0">
       <div className="mx-auto flex h-full max-w-2xl flex-col items-center justify-center gap-20">
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -6,9 +6,13 @@ export const SignIn: FC = () => {
   return (
     <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
       <div className="flex flex-col space-y-2 text-center">
-        <Icons.logo className="mx-auto h-6 w-6 sm:h-6 sm:w-6" />
+        <Icons.logo className="mx-auto h-6 w-6" />
+        <h1 className="text-2xl font-semibold tracking-tight">Welcome back!</h1>
+        <p className="mx-auto max-w-xs text-sm">
+          By continuing, you agree to setting up a Breadit account and Agreeing
+          to our User Agreement and Privacy Policy.
+        </p>
       </div>
-      SignIn
     </div>
   )
 }
